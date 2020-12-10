ec3a955562cd930fba57cbf42f714546230f16e5
nagarajug87 <goranti1987@gmail.com>
2020-12-10 20:11:32 +0530

    Custom Spartacus Changes

--- js-storefront/daspartacusstore/src/app/app.component.ts
+++ js-storefront/daspartacusstore/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-  
 })
 export class AppComponent {
   title = 'daspartacusstore';
--- js-storefront/daspartacusstore/src/assets/js/threekit_pdp.js
+++ js-storefront/daspartacusstore/src/assets/js/threekit_pdp.js
@@ -2,7 +2,6 @@ function threekit_pdp(assetId){
         let targetEl = document.getElementById("player");
         // Auth Tokens are Domain Bound
         let authToken = "0aac3498-bd82-4b70-893a-55d79b300607";
-        //let assetId = "314d31bc-2166-45cb-8258-f80dfc17cd1c";
         window.threekitPlayer({
             authToken : authToken,
             el : targetEl,
@@ -34,36 +33,5 @@ function threekit_pdp(assetId){
           .then((res) => res.json())
           .then((data) => data.products[0]);
       console.log("Product is:"+tkProduct.id);
-      threekit_pdp(tkProduct.id);
-
-        
-      /*  console.log("Fetching data...");
-    getJSON(url)
-        .then(data => console.log(data));*/
-
-        /*let response = await fetch(url);
-        let data = await response.json();
-        console.log(data.products[0]);*/
+      return tkProduct.id;
       }
-
-    
-
-      
-      
-      
-      /*function getJSONP(url, success) {
-
-        var ud = '_' + +new Date,
-            script = document.createElement('script'),
-            head = document.getElementsByTagName('head')[0] 
-                   || document.documentElement;
-    
-        window[ud] = function(data) {
-            head.removeChild(script);
-            success && success(data);
-        };
-    
-        script.src = url.replace('callback=?', 'callback=' + ud);
-        head.appendChild(script);
-    
-    }*/
